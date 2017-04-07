788d57c9351331b08705ee518fabc33fd4d9eb14
Christopher Groskopf <chrisgroskopf@gmail.com>
2017-04-07 08:11:24 -0500

    Compute pooled estimates.

--- atus.R
+++ atus.R
@@ -74,6 +74,19 @@ atus.anytime.occ.totals <- atus %>%
 
 write_csv(atus.anytime.occ.totals, "results/atus.anytime.occ.totals.csv")
 
+# Compute share of employees that worked at home for any time by industry
+atus.anytime.occ.shares <- atus %>%
+  filter(wageworkers) %>%
+  group_by(year, occupation, homeworkers) %>%
+  summarise(
+    n = sum(WT06) / 365
+  ) %>%
+  mutate(
+    share = n / sum(n)
+  )
+
+write_csv(atus.anytime.occ.shares, "results/atus.anytime.occ.shares.csv")
+
 # Count of fullday homeworkers by occupation
 atus.fullday.occ.totals <- atus %>%
   filter(homeworkers.fullday & wageworkers) %>%
@@ -84,3 +97,43 @@ atus.fullday.occ.totals <- atus %>%
 
 write_csv(atus.fullday.occ.totals, "results/atus.fullday.occ.totals.csv")
 
+# Create 3-year pooled ATUS data
+atus.pooled <- atus %>%
+  filter(FALSE)
+
+atus.pooled <- cbind(pool.year = character(), atus.pooled)
+
+for (year in 2004:2014) {
+  for (y in (year - 1):(year + 1)) {
+    temp <- atus %>%
+      filter(year == y)
+    
+    temp <- cbind(pool.year = as.character(year), temp)
+    
+    atus.pooled <- rbind(atus.pooled, temp)
+  }
+}
+
+atus.pooled$pool.year <- as.factor(atus.pooled$pool.year)
+
+# Pooled count and means for all employees that worked at home for any time
+atus.pooled.anytime.totals <- atus.pooled %>%
+  filter(homeworkers & wageworkers) %>%
+  group_by(pool.year) %>%
+  summarise(
+    n = sum(WT06) / 1095,
+    mean.mins = sum(workingfromhome * WT06) / sum(WT06)
+  )
+
+write_csv(atus.pooled.anytime.totals, "results/atus.pooled.anytime.totals.csv")
+
+# Pooled counts and means by occupation
+atus.pooled.anytime.occ.totals <- atus.pooled %>%
+  filter(homeworkers & wageworkers) %>%
+  group_by(pool.year, occupation) %>%
+  summarise(
+    n = sum(WT06) / 1095,
+    mean.mins = sum(workingfromhome * WT06) / sum(WT06)
+  )
+
+write_csv(atus.pooled.anytime.occ.totals, "results/atus.pooled.anytime.occ.totals.csv")
